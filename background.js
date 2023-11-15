318283c2f5134c985161c06f68ca785bf850fbaa
Shine Chang <shine00chang@gmail.com>
2023-11-14 16:36:49 -0800

    (fix) proper injection & tab detection

--- background.js
+++ background.js
@@ -1,11 +1,31 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, _) {
-	if (changeInfo.url) {
-		if (blockedUrls.some(el => changeInfo.url.includes(el))) {
-			chrome.tabs.executeScript(tabId, { file: "content.js" });
-		}
-	}
-}
-);
+
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => 
+{
+  /* Reference: https://developer.chrome.com/docs/extensions/reference/tabs/#event-onUpdated
+   * Only injects if "stats" is "complete"
+   *  ^ this fires when the page is done loading. you can find this on the linked page.
+   * This is necessary since "onUpdated" will be fired when the injection happens, leading to an infinite loop. */  
+  if (changeInfo.status !== 'complete') { return; }
+
+  const { url } = tab;
+
+  if (!blockedUrls.some(blocked => url.includes(blocked))) {
+    console.log("Imma let this slide: ", url);
+    return;
+  }
+
+  console.log("blocked url: ", url);
+
+  /* Reference: https://developer.chrome.com/docs/extensions/reference/scripting/#method-executeScript */
+  const injection = {
+    files: [ "content.js" ],
+    target: { tabId }
+  };
+  
+  chrome.scripting.executeScript(injection);
+});
+
 
 const blockedUrls = [
 	"facebook.com",
