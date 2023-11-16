cd4bb774f0192581b24fad75e072bfac16f3112c
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-11-15 21:08:43 -0800

    focus mode toggle

--- background.js
+++ background.js
@@ -1,29 +1,34 @@
+/* Ref: https://developer.chrome.com/docs/extensions/reference/storage/
+ * storage.sync is synced across browsers on multiple devices, local is not.
+ * Since focus mode's ideal behavior is device/session-specific, we use storage.localeCompare();
+ */
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+	const currentFocusMode = await chrome.storage.local.get(["focusMode"]);
 
+	/* Reference: https://developer.chrome.com/docs/extensions/reference/tabs/#event-onUpdated
+	 * Only injects if "stats" is "complete"
+	 *  ^ this fires when the page is done loading. you can find this on the linked page.
+	 * This is necessary since "onUpdated" will be fired when the injection happens, leading to an infinite loop. */
+	if (changeInfo.status !== 'complete') { return; }
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => 
-{
-  /* Reference: https://developer.chrome.com/docs/extensions/reference/tabs/#event-onUpdated
-   * Only injects if "stats" is "complete"
-   *  ^ this fires when the page is done loading. you can find this on the linked page.
-   * This is necessary since "onUpdated" will be fired when the injection happens, leading to an infinite loop. */  
-  if (changeInfo.status !== 'complete') { return; }
+	const { url } = tab;
 
-  const { url } = tab;
+	if (!blockedUrls.some(blocked => url.includes(blocked))) {
+		console.log("Imma let this slide: ", url);
+		return;
+	}
 
-  if (!blockedUrls.some(blocked => url.includes(blocked))) {
-    console.log("Imma let this slide: ", url);
-    return;
-  }
+	console.log("blocked url: ", url);
 
-  console.log("blocked url: ", url);
+	/* Reference: https://developer.chrome.com/docs/extensions/reference/scripting/#method-executeScript */
+	const injection = {
+		files: ["content.js"],
+		target: { tabId }
+	};
 
-  /* Reference: https://developer.chrome.com/docs/extensions/reference/scripting/#method-executeScript */
-  const injection = {
-    files: [ "content.js" ],
-    target: { tabId }
-  };
-  
-  chrome.scripting.executeScript(injection);
+	if (currentFocusMode.focusMode === true) {
+		chrome.scripting.executeScript(injection);
+	}
 });
 
 
---
+++ popup.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", async () => {
+	const focusToggle = document.getElementById("focus-toggle");
+	const currentFocusMode = await chrome.storage.local.get(["focusMode"]);
+
+	alert(currentFocusMode.focusMode);
+
+	// initialize checkbox value based on current focus mode
+	if (currentFocusMode.focusMode === true) {
+		focusToggle.checked = true;
+	}
+
+	focusToggle.addEventListener("click", async () => {
+		await chrome.storage.local.set({ focusMode: !currentFocusMode.focusMode })
+	});
+});
