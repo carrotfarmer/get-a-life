5cf64825f196148c74330c76304c263feed037f2
Shine Chang <shine00chang@gmail.com>
2023-11-27 21:11:02 -0800

    (ui) update

--- popup.js
+++ popup.js
@@ -1,22 +1,28 @@
-document.addEventListener("DOMContentLoaded", async () => {
-	const focusToggle = document.getElementById("focus-toggle");
-	const currentFocusMode = await chrome.storage.local.get(["focusMode"]);
+// DOM elements
+let toggle_elem;
+let message_elem;
 
-	//alert(currentFocusMode.focusMode);
+document.addEventListener("DOMContentLoaded", async () => 
+{
+  toggle_elem = document.getElementById("toggle");
+  message_elem = document.getElementById("message");
+	toggle_elem.addEventListener("click", onToggle);
 
 	// initialize checkbox value based on current focus mode
-	if (currentFocusMode.focusMode === true) {
-		focusToggle.checked = true;
-	}
-
-	focusToggle.addEventListener("click", async () => {
-		await chrome.storage.local.set({ focusMode: !currentFocusMode.focusMode })
-	});
+	const { focusMode } = await chrome.storage.local.get(["focusMode"]);
+  setMode(focusMode);
 });
 
-const onToggle = () => {
-  const checked = document.getElementById('toggle').checked;
-  console.log("toggled, checked: ", checked);
+const onToggle = async () => {
+	const { focusMode } = await chrome.storage.local.get(["focusMode"]);
+  setMode(!focusMode);
+}
 
-  // something else
+const setMode = async (mode) => {
+	await chrome.storage.local.set({ focusMode: mode });
+
+  toggle_elem.checked = mode;
+  message_elem.innerText = mode ? 
+    "Locked in!" :
+    "Need help focusing? Flip the switch!";
 }
